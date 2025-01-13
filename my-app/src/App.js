92f839e9f5cf4a6e525df7724c87d092146b3aee
Long <100302093+boiozzi@users.noreply.github.com>
2025-01-13 17:02:59 +0000

    initialize firebase.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBhAoHFS5fYA2J6dCrtlR98qZaWNbjhJV8",
+  authDomain: "test-app-long.firebaseapp.com",
+  projectId: "test-app-long",
+  storageBucket: "test-app-long.firebasestorage.app",
+  messagingSenderId: "1002591707963",
+  appId: "1:1002591707963:web:8e684b8742c797d6e24d1e",
+  measurementId: "G-77608YFV1C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 function App() {
   return (
     <div className="App">
